edf05b768ad0573604773001632349bf0e4ac557
104-wonohfor <thang20973@gmail.com>
2022-10-19 17:38:02 +0700

    19/10/2022 17:38:02

---
+++ kieu_du_lieu.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+   cout << "Kich thuoc cua char la: " << sizeof(char) <<" byte"<< endl; 
+   cout << "Kich thuoc cua int la: " << sizeof(int) <<" byte"<< endl; 
+   cout << "Kich thuoc cua short int la: " << sizeof(short int) <<" byte"<< endl; 
+   cout << "Kich thuoc cua long int la: " << sizeof(long int) <<" byte"<< endl; 
+   cout << "Kich thuoc cua float la: " << sizeof(float) <<" byte"<< endl; 
+   cout << "Kich thuoc cua double la: " << sizeof(double) <<" byte"<< endl; 
+   cout << "Kich thuoc cua wchar_t la: " << sizeof(wchar_t) <<" byte"<< endl;
+}
