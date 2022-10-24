295440e4c7b3397137ea0ddacefe54797db29e89
104-wonohfor <thang20973@gmail.com>
2022-10-24 09:02:20 +0700

    24/10/2022 09:02:20

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,39 +2,8 @@
 using namespace std;
 int main()
 {
-    int x,y;
-    cout<<"Input month: ";
-    cin >> x;
-    cout<<"Input year: ";
-    cin >>y;
+    int a,b,c,d;
+    a = 4, b=2; c= 8; d=3;
 
-    switch (x)
-    {
-    case 1: 
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12: cout<<x<<"/"<<y<<" has 31 days"<<endl; break;
-
-    case 4:
-    case 6:
-    case 9:
-    case 11: cout<<x<<"/"<<y<<" has 30 days"<<endl; break;
-
-    case 2:
-        if ((y%4==0 && y%100!=0) || (y%400==0))
-        {
-            cout<<x<<"/"<<y<<" has 29 days"<<endl; 
-        }
-        else
-        {
-            cout<<x<<"/"<<y<<" has 28 days"<<endl;
-        }
-        break;        
-    default:
-        cout<<"Month not exists";
-        break;
-    }
+    cout<<(d>(c>(a>b?a:b)?c:(a>b?a:b))?d:(c>(a>b?a:b)?c:(a>b?a:b)))<<"is the greatest"<<endl;
 }
