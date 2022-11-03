98323d2f13ce5b1c05d36e33dcd1c23081c4cdf8
104-wonohfor <thang20973@gmail.com>
2022-11-03 17:15:48 +0700

    03/11/2022 17:15:48

--- function.cpp
+++ function.cpp
@@ -89,3 +89,39 @@ int main()
     }
 }
 
+
+#include <iostream>
+using namespace std;
+
+float tinh(float a, float b,float &tong,float &hieu)
+{
+    tong = a+b;
+    hieu = a-b;
+    cout<<a<< " + "<<b<<" = "<<tong<<endl;
+    cout<<a<< " - "<<b<<" = "<<hieu<<endl;
+}
+int main()
+{    
+    float a =4.5, b=6.0,tong,hieu;
+    tinh(a,b,tong,hieu);
+    float tong_1 = tong;
+    cout<<tong_1<<endl;
+    float c =6.5,d=6.0;
+    tinh(c,d,tong,hieu);
+    float tong_moi =tong;
+    cout<<tong_moi<<endl;
+}
+
+
+#include <iostream>
+using namespace std;
+
+void printValue(int x , int y=5) 
+{
+	cout << "x: " << x << endl;
+	cout << "y: " << y << endl;
+}
+int main(){
+    printValue(4);
+    printValue(4,6);
+}
