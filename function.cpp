f1f8b004aaaeef0981f1b57d1f0c33f362729def
104-wonohfor <thang20973@gmail.com>
2022-11-03 16:17:30 +0700

    03/11/2022 16:17:29

--- function.cpp
+++ function.cpp
@@ -55,3 +55,37 @@ int main()
     cout<<add(a,b);
 }
 
+
+#include <iostream>
+using namespace std;
+
+int prime(int n)
+{
+    int i;
+    for (int i = 2; i <= n; ++i)
+    {
+        if (n%i==0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    
+}
+
+int main()
+{
+    int n;
+    int flag = 0;
+    cout<<"Nhap so: ";
+    cin>>n;
+
+    flag = prime(n);
+    if (flag == 0){
+        cout<<n<<" is a prime number"<<endl;
+    }
+    else{
+    cout<<n<<" is not a prime number"<<endl;
+    }
+}
+
