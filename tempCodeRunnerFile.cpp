4ca63414f971e5952cf4e72604d5e0fcf21d0a39
104-wonohfor <thang20973@gmail.com>
2022-11-03 15:50:10 +0700

    03/11/2022 15:50:10

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
-int DisplayNumber(float a, float b)
+
+int prime(int n)
 {
-    cout<<"The int number is "<<a<<endl;
-    cout<<"The float number is "<<b<<endl;
+    int i;
+    for (int i = 2; i <= n; ++i)
+    {
+        if (n%i==0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    
 }
-float add(double a,double b)
-{
-    float sum = a+b;
-    return sum;
-}
+
 int main()
 {
-    float a = 4;
-    float b = 6.3;
-    DisplayNumber(a,b);
-    cout<<"a + b = ";
-    cout<<add(a,b);
+    int n;
+    int flag = 0;
+    cout<<"Nhap so: ";
+    cin>>n;
+
+    flag = prime(n);
+    if (flag == 0){
+        cout<<n<<" is a prime number"<<endl;
+    }
+    else{
+    cout<<n<<" is not a prime number"<<endl;
+    }
 }
