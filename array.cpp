6fdaebbeca2c2162ebc7002b8eb8bb1fa9531cad
104-wonohfor <thang20973@gmail.com>
2022-11-10 11:36:55 +0700

    10/11/2022 11:36:55

--- array.cpp
+++ array.cpp
@@ -191,3 +191,150 @@ int main(){
     }
 }
 
+// Cho trước mảng a, số lượng phần tử na và mảng b số lượng phần tử nb. 
+// Gộp 2 mảng trên theo tứ tự đó thành mảng c, số lượng phần tử nc.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int na = 6,nb = 4;
+    int arrayA[na] = {3,4,7,8,3,8};
+    int arrayB[nb] = {2,4,5,1};
+    int nc = na +nb;
+    int arrayC[nc] = {};
+
+    for (int i = 0; i < na; i++){
+        arrayC[i] = arrayA[i];
+    }
+     
+    for (int i = 0; i < nb; i++){
+        arrayC[i+na] = arrayB[i];
+    }
+    
+    cout<<"arrayC: ";
+    for (int i = 0; i < nc; i++){
+        cout<<arrayC[i]<<" ";
+    }   
+} // Run this code 1 time without error ://. I feel....
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int na = 6,nb = 5;
+    char arrayA[na] = "VoDuc";
+    char arrayB[nb] = "Thang";
+    int nc = na +nb;
+    char arrayC[nc] = {};
+
+    for (int i = 0; i < na; i++){
+        arrayC[i] = arrayA[i];
+    }
+     
+    for (int i = 0; i < nb; i++){
+        arrayC[i+na] = arrayB[i];
+    }
+    
+    cout<<"arrayC: ";
+    for (int i = 0; i < nc; i++){
+        cout<<arrayC[i];
+    }  
+}
+
+// Cho trước mảng a có n phần tử. Tìm giá trị lớn nhất trong mảng a (gọi là max).
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arrayA [9] = {2,5,7,19,12,11,10,5};
+    int size = sizeof(arrayA)/sizeof(arrayA[0]);
+    int max = arrayA[0];
+    for (int i = 0; i < size; i++){
+        if (arrayA[i]>max){
+            max = arrayA[i];
+        } 
+    }
+    cout<<max;
+} // First time without error again. I... uhm....
+
+// Cho trước mảng a kích thước n. 
+// Hãy sắp xếp mảng a đó sao cho các phần tử có giá trị tăng dần.
+
+/// Sắp xếp nổi bọt (Bubble Sort)
+#include<iostream>
+using namespace std;
+
+void HoanVi(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+    #define n 10
+    int arrayA[n] = {7,5,9,19,12,5,2,5,5,10};
+
+    cout<<"arrayA: ";
+                for (int k = 0; k < n; k++){
+                    cout<<arrayA[k]<<" ";
+                }
+
+    for (int i = 0;i<n;i++){
+        for (int j = 0; j < n; j++){
+            if (arrayA[i] < arrayA[j]){
+                HoanVi(arrayA[i],arrayA[j]);
+                cout<<"\narrayA: ";
+                for (int k = 0; k < n; k++){
+                    cout<<arrayA[k]<<" ";
+                }
+            }
+        }
+    }
+
+    cout<<"\nlast arrayA: ";
+    for (int i = 0; i < n; i++){
+        cout<<arrayA[i]<<" ";
+    }
+}
+
+// Quick Sort.
+
+#include<iostream>
+using namespace std;
+
+void swap(int *a,int *b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+int partition(int arr[],int l,int h){
+	int x=arr[h];
+	int i=l-1;
+	for(int j=l; j<=h-1; j++){
+		if(arr[j]<=x){
+			i++;
+			swap(&arr[i],&arr[j]);
+		}
+	}
+	swap(&arr[i+1],&arr[h]);
+	return(i+1);
+}
+void quicksort(int arr[],int l,int h){
+	if(l<h){
+		int p=partition(arr,l,h);
+		quicksort(arr,l,p-1);
+		quicksort(arr,p+1,h);
+	}
+}
+int main(){
+    #define n 10
+    int arrayA[n] = {7,5,9,19,12,5,2,5,5,10};
+    partition(arrayA,0,n);
+    quicksort(arrayA,0,n);
+    cout<<"\narrayA: ";
+        for (int k = 0; k < n; k++){
+        cout<<arrayA[k]<<" ";
+        }
+}
